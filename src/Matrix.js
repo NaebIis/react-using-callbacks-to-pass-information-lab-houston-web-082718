5f801213641b3c6392daad1ad58487041f640d6b
Naeblis <johnmlowery@gmail.com>
2018-10-30 10:35:52 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,36 +1,59 @@
-import React, { Component } from 'react';
-import chromeBoi from './data.js'
-import Cell from './Cell.js'
-import ColorSelector from './ColorSelector.js'
+import React, { Component } from "react";
+import chromeBoi from "./data.js";
+import Cell from "./Cell.js";
+import ColorSelector from "./ColorSelector.js";
 
 export default class Matrix extends Component {
-
   constructor() {
-    super()
+    super();
+    this.state = {
+      color: "#f00"
+    };
   }
+  // ----------------------------------------------
+  changeColor = (nuColor, e) => {
+    this.setState({
+      color: nuColor
+    });
+    console.log(this.state.color);
+  };
 
-  genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
-  )
+  currentColor = () => {
+    return this.state.color;
+  };
+  // --------------------------------------------------
+  genRow = vals =>
+    vals.map((val, idx) => (
+      <Cell
+        key={idx}
+        color={val}
+        // ---------------------------------------------------
+        currentColor={this.currentColor}
+        // ---------------------------------------------------
+      />
+    ));
 
-  genMatrix = () => (
-    this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
-  )
-
+  genMatrix = () =>
+    this.props.values.map((rowVals, idx) => (
+      <div key={idx} className="row">
+        {this.genRow(rowVals)}
+      </div>
+    ));
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
-        <div id="matrix">
-          {this.genMatrix()}
-        </div>
+        <ColorSelector
+          // --------------------------------------------
+          changeColor={this.changeColor.bind(this)}
+          // --------------------------------------------
+        />
+        <div id="matrix">{this.genMatrix()}</div>
       </div>
-    )
+    );
   }
-
 }
 
 Matrix.defaultProps = {
   values: chromeBoi
-}
+};
